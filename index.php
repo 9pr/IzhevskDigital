e2608c4454d31a3c5fa819ba183f81a49a3178f5
FOX\Антон <anton@9pr.ru>
2019-07-20 17:25:03 +0400

    Начальная структура pwa приложения

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>NextLevel</title>
+    <meta name="theme-color" content="#e7e7e7"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="/assets/icons/icon-72x72.png">
+    <link rel="manifest" href="/manifest.json">
+  </head>
+  <body>
+    <p>NextLevel</p>
+    <script>
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('Registration success. Scope is ', reg.scope))
+        .catch(err => console.log('Registration failed. ', err));
+      }
+    </script>
+  </body>
+</html>
---
+++ sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'nextlevel';
+const resourcesToCache = [
+  '/',
+  '/manifest.json',
+  '/assets/icons/icon-72x72.png',
+  '/assets/icons/icon-96x96.png',
+  '/assets/icons/icon-128x128.png',
+  '/assets/icons/icon-144x144.png',
+  '/assets/icons/icon-152x152.png',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-384x384.png',
+  '/assets/icons/icon-512x512.png',
+
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(resourcesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
