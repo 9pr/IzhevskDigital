328f6d10e7b7d1b7c32c24e4a620508bc8479943
LShirokova <79120151523@ya.ru>
2019-07-21 15:21:11 +0400

    Обработка кнопок

--- .distr/blocks/user-panel/block.php
+++ .distr/blocks/user-panel/block.php
@@ -1,4 +1,4 @@
 <div class="user-panel">
-	<button class="user-panel__btn user-panel__btn_yes"><i class="icon-emo-happy"></i> <br>Вижу свободные места</button>
-	<button class="user-panel__btn user-panel__btn_no"><i class="icon-emo-angry"></i> <br>Не вижу свободных мест</button>
+	<button id="user-panel__btn_yes" class="user-panel__btn user-panel__btn_yes" data-point-status="on"><i class="icon-emo-happy"></i> <br>Вижу свободные места</button>
+	<button id="user-panel__btn_no" class="user-panel__btn user-panel__btn_no" data-point-status="off"><i class="icon-emo-angry"></i> <br>Не вижу свободных мест</button>
 </div>
