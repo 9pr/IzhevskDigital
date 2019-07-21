f1c895d7af69993561f7c93009c3c8f2b39e20b4
FOX\Антон <anton@9pr.ru>
2019-07-21 11:39:52 +0400

    Добавили функционал для муниципалитета

--- classes/Api.php
+++ classes/Api.php
@@ -21,6 +21,51 @@ class Api {
         return $result;
     }
 
+    public function getStatisticsOccupation () {
+        $sql = 'SELECT *, COUNT(`idParkingStatisticsOccupation`) AS records, SUM(`statusStatisticsOccupation`) AS wasFree FROM `statisticsoccupation` 
+        INNER JOIN `parking` ON `idParkingStatisticsOccupation` = `idParking` GROUP BY `idParkingStatisticsOccupation`';
+        //формируем массив для парковок
+        $parking = [];
+
+        $sth = $this->db->prepare($sql);
+        $sth->execute();
+
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+
+            /**
+             * [idParking] - идентификатор парковки
+             * [coordinatesParking] - координаты парковки
+             * [records] - всего записей по парковке
+             * [wasFree] - сколько раз она была вободна
+             * соотношение wasFree/records определяет загруженность менее 30% зеленая, более 70% красная, остальное желтое
+            */
+            if ($row["wasFree"]/$row["records"] < 0.3) {
+                $loadType = "islands#greenIcon";
+            }
+            elseif ($row["wasFree"]/$row["records"] > 0.7) {
+                $loadType = "islands#redIcon";
+            }
+            else {
+                $loadType = "islands#yellowIcon";
+            }
+            $parking[] = '{"type": "Feature", "id": '.$row['idParking'].', "geometry": {"type": "Point", "coordinates": ['.$row['coordinatesParking'].']}, "options": {"preset": "'.$loadType.'"}, "properties": {"balloonContentHeader": "Заголовок балуна", "balloonContentBody": "<a href=\"statisticsOccupationDetailed.php?idParking='.$row['idParking'].'\" target=\"_blank\">Подробная статистика</a>", "balloonContentFooter": "Контент балуна подвал", "clusterCaption": "Метка", "hintContent": "Текст подсказки"}}';
+
+
+
+
+
+
+            // print_r($current);
+        }
+
+        // foreach ($this->db->query($sql, PDO::FETCH_ASSOC) as $row) {
+
+        // }
+        echo '{"type": "FeatureCollection","features": ['.join (', ', $parking).']}';
+        return '';
+        return $result;
+    }
+
     public function setParking() {
         $stmt = $this->db->prepare("INSERT INTO parking (coordinatesParking, typeParking, descriptionParking) VALUES (:coordinatesParking,:typeParking,:descriptionParking)");
         $stmt->bindParam(':coordinatesParking', $coordinatesParking, PDO::PARAM_STR);
--- index.php
+++ index.php
@@ -1,37 +1,3 @@
 <script>
   window.location.replace('https://nlevel.yodata.ru/public'); 
 </script>
-
-
-<!-- 
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>NextLevel</title>
-    <meta name="theme-color" content="#e7e7e7"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="/assets/icons/icon-72x72.png">
-    <link rel="manifest" href="/manifest.json">
-  </head>
-  <body>
-    <p>NextLevel</p>
-    <script>
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('Registration success. Scope is ', reg.scope))
-        .catch(err => console.log('Registration failed. ', err));
-      }
-
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(  position => {
-          console.log(position);
-        });
-      }
-
-
-
-
-    </script>
-  </body>
-</html> -->
