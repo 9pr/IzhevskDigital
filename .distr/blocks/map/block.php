85b97869e6aac1b63485d767d569ce7ba93a2436
LShirokova <79120151523@ya.ru>
2019-07-21 07:03:12 +0400

    Поиск ближайшей парковки

--- .distr/blocks/map/block.php
+++ .distr/blocks/map/block.php
@@ -1,1 +1,7 @@
-<div id="map" class="map"></div>
+<div class="map">
+	<!--div class="map__search">
+		<input class="map__search-input js-map__search-input" type="search" name="map__search-input">
+	</div-->
+
+	<div id="map__widget" class="map__widget"></div>
+</div>
--- .distr/blocks/map/script.js
+++ .distr/blocks/map/script.js
@@ -1,78 +1,92 @@
-ymaps.ready(initMap);
+ymaps.ready(init);
 
+function init() {
+    var geolocation = ymaps.geolocation,
+        myMap = new ymaps.Map('map__widget', {
+        center: [56.852593, 53.204843], // Ижевск
+        zoom: 18,
+        controls: []
+    }, {
+            searchControlProvider: 'yandex#search'
+        });
 
-
-function initMap() {
-	var myMap = new ymaps.Map('map', {
-		center: [56.852593, 53.204843], // Ижевск
-    zoom: 12,
-    controls: []
-  }, {
-    searchControlProvider: 'yandex#search'
-  });
-
-
-	// Создадим панель маршрутизации.
-  routePanelControl = new ymaps.control.RoutePanel({
-    options: {
-      // Добавим заголовок панели.
-      showHeader: true,
-      title: 'Постройте маршрут'
-    }
-  });
-
-
-  // Пользователь сможет построить только автомобильный маршрут.
-  routePanelControl.routePanel.options.set({
-    types: {auto: true}
-  });
-
-  myMap.controls.add(routePanelControl);
-
-
-
-
-	// Получим ссылку на маршрут.
-  routePanelControl.routePanel.getRouteAsync().then(function (route) {
-  	// Повесим обработчик на событие построения маршрута.
-    route.model.events.add('requestsuccess', function () {
-    	var activeRoute = route.getActiveRoute();
-    	// Нужно дождаться ответа от сервера и только потом обрабатывать полученные результаты.
-    	if (activeRoute) {
-    		// Запрашиваем парковки
-    		jQuery.getJSON('js/parkings.json', function (json) {
-			  	var parkings,
-							endPoint,
-							points = route.getWayPoints(),
-	        		lastPoint = points.get(1).properties.get("coordinates"); // TODO координаты определяются НЕ правильно
-	        		//console.log(points.get(1).properties.geometry.getCoordinates());
-
-			    parkings = ymaps.geoQuery({
-			    	type: 'FeatureCollection',
-			    	features: json
-			    }).addToMap(myMap);
-
-			    //points.get(0).options.set('preset', 'islands#redStretchyIcon');
-
-
-			    // С помощью обратного геокодирования найдем ближайшую парковку.
-					endPoint = ymaps.geoQuery(ymaps.geocode([lastPoint], {kind: 'street'}))
-					// Будем открывать балун парковки, который ближе всего к парковке
-					.then(parkings.getClosestTo(lastPoint).balloon.open());
-
-					route.editor.start(
-						{addWayPoints: true, removeWayPoints: true}
-					);
-
-					route.editor.stop();
-					//console.log(parkings.getClosestTo(lastPoint).geometry.getCoordinates());
-			  });
-
-      }
+    // Добавляю блок поиска на сайт
+    var mySearchControl = new ymaps.control.SearchControl({
+        options: {
+            noPlacemark: true
+        }
     });
-  });
+    myMap.controls.add(mySearchControl);
+
+
+    // Запрашиваю парковки
+    jQuery.getJSON('js/parkings.json', function (json) {
+        var parkings,
+            parking;
+
+        // Вывожу парковки на карте
+        parkings = ymaps.geoQuery({
+            type: 'FeatureCollection',
+            features: json
+        }).addToMap(myMap);
+
+        // Получаю координаты поиска
+        mySearchControl.events.add('resultselect', function (e) {
+            var index = e.get('index');
+            mySearchControl.getResult(index).then(function (res) {
+                var routetEnd = res.geometry.getCoordinates();
+
+                // С помощью обратного геокодирования найдем ближайшую парковку
+                parking = ymaps.geoQuery(ymaps.geocode(routetEnd, {kind: 'street'}))
+                // Нужно дождаться ответа от сервера и только потом обрабатывать полученные результаты.
+                .then( parkings.getClosestTo(routetEnd).balloon.open() );
+
+                console.log(parking.geometry.getCoordinates())
+
+
+                // Геолокация
+                geolocation.get({
+                    provider: 'yandex',
+                    mapStateAutoApply: true
+                }).then(function (result) {
+                    var userPosition = result.geoObjects.position;
+                    console.log(userPosition, parking, routetEnd);
+
+
+                    // Строю маршрут
+                    var multiRoute = new ymaps.multiRouter.MultiRoute({
+                        referencePoints: [
+                            userPosition,
+                            parking,
+                            routetEnd
+                        ],
+                        params: {
+                            routingMode: 'masstransit'
+                        }
+                    });
+
+                    ymaps.modules.require([
+                        'MultiRouteColorizer'
+                    ], function (MultiRouteColorizer) {
+                        // Создаем объект, раскрашивающий линии сегментов маршрута.
+                        new MultiRouteColorizer(multiRoute);
+                    });
+
+
+                    // Добавляем мультимаршрут на карту.
+                    myMap.geoObjects.add(multiRoute);
+
+                });
 
 
 
 
+
+            });
+
+
+        });
+
+
+    });
 }
